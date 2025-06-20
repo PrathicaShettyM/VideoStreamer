65ae843d32f34c31ce1d9e9235de1bcb588a36e0
Prathica Shetty M <123286880+PrathicaShettyM@users.noreply.github.com>
2025-06-20 17:51:01 +0530

    Util codes of custom responses and errors

--- server/index.js
+++ server/index.js
@@ -1,10 +1,28 @@
 import express from "express";
 import dbConnect from "./db/dbConnect.js";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 dotenv.config();
 
-
 const app = express();
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+})); // .use() => used with middlewares or configurations
+
+app.use(express.json({
+    limit: "16kb"
+}));
+
+app.use(express.urlencoded({
+    extended: true,
+    limit: "16kb"
+}));
+
+app.use(express.static("public"));
+
+app.use(cookieParser());
 
 
 // db connection promise
