5d71b43f39a3d3ea5aad35691b6811bc4c2ad4be
Prathica Shetty M <123286880+PrathicaShettyM@users.noreply.github.com>
2025-06-22 12:17:16 +0530

    Added subscription schema

---
+++ server/models/subscription.model.js
@@ -0,0 +1,14 @@
+import mongoose, {Schema} from "mongoose";
+
+const subscriptionSchema = new Schema({
+    subscriber: {
+        type: Schema.Types.ObjectId, // one who is subscribing => audience
+        ref: "User"
+    },
+    channel: {
+        type: Schema.Types.ObjectId, // one to whom 'subscriber' is subscribing => youtuber
+        ref: "User"
+    }
+});
+
+const Subscription = mongoose.model("Subscription", subscriptionSchema);
