f5f18f3869b0b9dd6f92674a0d37beadad4e0be7
Prathica Shetty M <123286880+PrathicaShettyM@users.noreply.github.com>
2025-06-22 11:24:02 +0530

    Added refresh token endpoint

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -69,23 +69,25 @@ userSchema.methods.generateAccessToken = function () {
         email: this.email,
         username: this.username,
         fullname: this.fullname,
-    }),
+    },
     process.env.ACCESS_TOKEN_SECRET,
     {
         expiresIn: process.env.ACCESS_TOKEN_EXPIRY
     }
+  );  
 }
 
 // refresh token
 userSchema.methods.generateRefreshToken = function () {
     return jwt.sign({
         _id: this._id,
-    }),
+    },
     process.env.REFRESH_TOKEN_SECRET,
     {
         expiresIn: process.env.REFRESH_TOKEN_EXPIRY
     }
-}
+    );
+};
 
 export const User = mongoose.model("User", userSchema);
 // jwt: bearer token
