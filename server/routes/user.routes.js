bc7908ddcf77e23f5e511a0ca811602b50d9c97d
Prathica Shetty M <123286880+PrathicaShettyM@users.noreply.github.com>
2025-06-27 05:58:00 +0530

    Added routes for remaining routes for user controller

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,5 +1,17 @@
 import {Router} from "express";
-import { loginUser, logoutUser, registerUser, refreshToken } from "../controllers/user.controller.js";
+import { 
+    loginUser, 
+    logoutUser, 
+    registerUser, 
+    refreshToken, 
+    changeCurrentPassword, 
+    getCurrentUser, 
+    updateAccountDetails, 
+    updateUserAvatar, 
+    updateUserCoverImage, 
+    getUserChannelProfile, 
+    getWatchHistory 
+} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js"; 
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -25,5 +37,14 @@ router.route("/login").post(loginUser)
 // private routes
 router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/refresh-token").post(refreshToken);
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+router.route("/current-user").post(verifyJWT, getCurrentUser);
+router.route("/update-account").patch(verifyJWT, updateAccountDetails); // patch: becoz update is happening so
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
+router.route("/cover-image").patch(verifyJWT, upload.single("/coverImage"), updateUserCoverImage);
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
+router.route("/history").get(verifyJWT, getWatchHistory);
+
+
 
 export default router;
